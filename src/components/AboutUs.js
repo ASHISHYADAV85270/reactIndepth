5505305c1983a3eca2a2261791dfbac75b7c9ab0
Ashish Yadav <ashish.yadav@aquera.com>
2024-03-12 23:53:39 +0530

    done with basic state management thing

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -4,7 +4,6 @@ const AboutUs = () => {
   return (
     <div>
       <h1>About Us Page</h1>
-      <User name="Ashish Yadav" location="Moradabad" mbn="85xx086883" />
       <UserClass name="Palak Yadav" location="Delhi" mbn="12943523u3" />
     </div>
   );
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -2,16 +2,27 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      count: 0,
+      email: "palak@gmail.com",
+    };
   }
 
   render() {
     const { name, location, mbn } = this.props;
-    console.log(name, location, mbn);
+    const { count, email } = this.state;
     return (
       <div className="user-card">
+        <h1> {this.state.count} </h1>
+        <h1> {count} </h1>
+        <h1> {this.state.email} </h1>
+        <h1> {email} </h1>
+
         <h2>{name} </h2>
         <h4>{location} </h4>
         <h5>{mbn} </h5>
+
+        <button onClick={() => {}}>Count Increase</button>
       </div>
     );
   }
