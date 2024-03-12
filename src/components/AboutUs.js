0ec7e6d6d0b7631678fa69ac648c33de066fa9ad
Ashish Yadav <ashish.yadav@aquera.com>
2024-03-12 23:56:53 +0530

    understood component did mount

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,12 +1,25 @@
-import User from "./User";
+import React from "react";
 import UserClass from "./UserClass";
-const AboutUs = () => {
-  return (
-    <div>
-      <h1>About Us Page</h1>
-      <UserClass name="Palak Yadav" location="Delhi" mbn="12943523u3" />
-    </div>
-  );
-};
+
+class AboutUs extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("Parent Constructor");
+  }
+
+  componentDidMount() {
+    console.log("Parent Did Mount");
+  }
+
+  render() {
+    console.log("Parent Render");
+    return (
+      <div>
+        <h1>About Us Page</h1>
+        <UserClass name="Palak Yadav" location="Delhi" mbn="12943523u3" />
+      </div>
+    );
+  }
+}
 
 export default AboutUs;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -2,27 +2,20 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      count: 0,
-      email: "palak@gmail.com",
-    };
+    console.log("Child Constructor");
   }
-
+  componentDidMount() {
+    console.log("Child Did Mount");
+  }
   render() {
     const { name, location, mbn } = this.props;
-    const { count, email } = this.state;
+    console.log("Child Render");
+
     return (
       <div className="user-card">
-        <h1> {this.state.count} </h1>
-        <h1> {count} </h1>
-        <h1> {this.state.email} </h1>
-        <h1> {email} </h1>
-
         <h2>{name} </h2>
         <h4>{location} </h4>
         <h5>{mbn} </h5>
-
-        <button onClick={() => {}}>Count Increase</button>
       </div>
     );
   }
